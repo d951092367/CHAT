93bac5f4b470bb92b06f5076d051a2d18ca55e2f
Administrator <Administrator@BF-202202192129>
2022-10-05 21:29:59 +0800

    添加项目文件。

---
+++ resource.h
@@ -0,0 +1,31 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 mfcCHARCLIENT.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCCHARCLIENT_DIALOG        102
+#define IDR_MAINFRAME                   128
+#define IDC_SEND_BTN                    1000
+#define IDC_CLEARMSG_BTN                1001
+#define IDC_DISCONNECT_BTN              1002
+#define IDC_CONNECT_BTN                 1003
+#define IDC__SENDMSG_EDIT               1005
+#define IDC_PORT_EDIT                   1006
+#define IDC_COLOR_COMBO                 1009
+#define IDC_LIST3                       1010
+#define IDC_AUTOSEND_RADIO              1011
+#define IDC_AUTOSENMSG_EDIT             1012
+#define IDC_IPADDRESS                   1013
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1014
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
